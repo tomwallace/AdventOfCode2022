dd8426fe93d30f9f6dc5962850af5c899fedac59
Tom Wallace <thomaspwallace@gmail.com>
2022-12-06 09:00:32 -0500

    Added Day 6 C Sharp solutions and tests

---
+++ AdventOfCode2022.CSharp.Tests/DaySixTests.cs
@@ -0,0 +1,35 @@
+﻿using AdventOfCode2022.CSharp.Six;
+
+namespace AdventOfCode2022.CSharp.Tests;
+
+public class DaySixTests
+{
+    [Theory]
+    [InlineData("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4, 7)]
+    [InlineData("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14, 19)]
+    public void CountOverlapSections(string input, int numberOfChars, int expected)
+    {
+        var sut = new DaySix();
+        var result = sut.FindRepeatMarker(input, numberOfChars);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void PartA_Actual()
+    {
+        var sut = new DaySix();
+        var result = sut.PartA();
+
+        Assert.Equal("1282", result);
+    }
+
+    [Fact]
+    public void PartB_Actual()
+    {
+        var sut = new DaySix();
+        var result = sut.PartB();
+
+        Assert.Equal("3513", result);
+    }
+}
--- AdventOfCode2022.FSharp/Program.fs
+++ AdventOfCode2022.FSharp/Program.fs
@@ -8,6 +8,7 @@ Day02.Execute
 Day03.Execute
 Day04.Execute
 Day05.Execute
+Day06.Execute
 Day99.Execute
 |]
 |> Array.iter id
